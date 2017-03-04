4551006dbbe14d2f08df27831cfc0441ae9178da
xiaozhuyfk <xiaozhuyfk@gmail.com>
2017-03-03 22:40:07 -0500

    pr

--- pagerank/page_rank.cpp
+++ pagerank/page_rank.cpp
@@ -82,6 +82,7 @@ void pageRank(Graph g, double* solution, double damping, double convergence) {
             }
             solution[i] = (damping * solution[i]) + (1.0 - damping) / numNodes;
 
+            #pragma omp parallel for
             for (int j = 0; j < disjoint.size(); j++) {
                 solution[i] += damping * old[disjoint[j]] / numNodes;
             }
