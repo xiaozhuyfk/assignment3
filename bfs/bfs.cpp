b9ae72a02f72fe11e131a976c18b717f19085751
xiaozhuyfk <xiaozhuyfk@gmail.com>
2017-03-04 08:00:15 -0500

    bfs

--- bfs/bfs.cpp
+++ bfs/bfs.cpp
@@ -31,7 +31,7 @@ void top_down_step(
         vertex_set* new_frontier,
         int* distances) {
 
-    #pragma omp parallel for
+    //#pragma omp parallel for
     for (int i = 0; i < frontier->count; i++) {
 
         int node = frontier->vertices[i];
