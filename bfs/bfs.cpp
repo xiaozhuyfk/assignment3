5eea7eb4a09cb3605b6ec86f19e5176bc2b6a808
xiaozhuyfk <xiaozhuyfk@gmail.com>
2017-03-06 01:56:11 -0500

    bfs

--- bfs/bfs.cpp
+++ bfs/bfs.cpp
@@ -57,7 +57,6 @@ void top_down_step(
     */
 
     int num_threads = omp_get_max_threads();
-    printf("%d\n", num_threads);
     int *dist_frontier = (int *) malloc(sizeof(int) * num_threads * g->num_nodes);
     int frontier_size[num_threads];
     memset(frontier_size, 0, num_threads * sizeof(int));
