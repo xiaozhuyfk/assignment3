0388ba20eeb5c91f873a312dd8d1c3d4c9a0072a
xiaozhuyfk <xiaozhuyfk@gmail.com>
2017-03-06 12:43:45 -0500

    bfs

--- bfs/bfs.cpp
+++ bfs/bfs.cpp
@@ -159,7 +159,7 @@ int bottom_up_step(
                     if (distances[in] == distance) {
                         distances[node] = distances[in] + 1;
                         frontier_size[i]++;
-                        break;
+                        return 1;
                     }
                 }
             }
