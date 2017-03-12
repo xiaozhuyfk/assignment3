2bd9660e6df7030c782f0d688f20022d667457df
xiaozhuyfk <xiaozhuyfk@gmail.com>
2017-03-11 19:10:16 -0500

    pr

--- part2/pagerank/page_rank.cpp
+++ part2/pagerank/page_rank.cpp
@@ -18,6 +18,56 @@ double compute_disjoint_weight(
         double damping,
         double *old) {
 
+    int total_vertices = g.total_vertices();
+    std::vector<double*> disjoint_send_bufs;
+    std::vector<int> disjoint_send_idx;
+    std::vector<double*> disjoint_recv_bufs;
+
+    MPI_Request* disjoint_send_reqs = new MPI_Request[g.world_size];
+
+    // Phase 1 : update disjoint weight
+    // Calculate local disjoint weight
+    double disjoint_weight = 0.;
+    #pragma omp parallel for reduction(+:disjoint_weight)
+    for (std::size_t j = 0; j < g.disjoint.size(); j++) {
+        disjoint_weight += damping * old[g.disjoint[j]] / total_vertices;
+    }
+    //pass local disjoint
+    double* disjoint_send_buf = new double[1];
+
+    for (int i = 0; i < g.world_size; i++) {
+        if (i != g.world_rank) {
+            disjoint_send_bufs.push_back(disjoint_send_buf);
+            disjoint_send_idx.push_back(i);
+            disjoint_send_buf[0] = disjoint_weight;
+            MPI_Isend(disjoint_send_buf, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, &disjoint_send_reqs[i]);
+        }
+
+    }
+    //receive and update local disjoint
+
+    for (int i = 0; i < g.world_size; i++) {
+        if (i!=g.world_rank) {
+            MPI_Status status;
+            double* recv_buf = new double[1];
+            disjoint_recv_bufs.push_back(recv_buf);
+
+            MPI_Recv(recv_buf, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, &status);
+            disjoint_weight += recv_buf[0];
+        }
+    }
+
+    // clear disjoint buf
+    delete(disjoint_send_buf);
+
+    for (size_t i = 0; i < disjoint_recv_bufs.size(); i++) {
+        delete(disjoint_recv_bufs[i]);
+    }
+
+    delete(disjoint_send_reqs);
+    return disjoint_weight;
+
+    /*
     int totalVertices = g.total_vertices();
 
     // Calculate local disjoint weight
@@ -50,11 +100,59 @@ double compute_disjoint_weight(
     }
 
     return total_weight;
+    */
 }
 
 
 double compute_global_diff(DistGraph &g, double *solution, double *old) {
 
+    std::vector<double*> converge_send_bufs;
+    std::vector<int> converge_send_idx;
+    std::vector<double*> converge_recv_bufs;
+
+    MPI_Request* converge_send_reqs = new MPI_Request[g.world_size];
+
+    // Calculate local convergence
+    double diff = 0.;
+    #pragma omp parallel for reduction(+:diff)
+    for (int i = 0; i < g.vertices_per_process; i++) {
+        diff += std::abs(solution[i] - old[i]);
+    }
+
+    // Pass local converge score
+    double* converge_send_buf = new double[1];
+    for (int i = 0; i < g.world_size; i++) {
+        if (i != g.world_rank) {
+            converge_send_bufs.push_back(converge_send_buf);
+            converge_send_idx.push_back(i);
+            converge_send_buf[0] = diff;
+            MPI_Isend(converge_send_buf, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, &converge_send_reqs[i]);
+        }
+
+    }
+    //receive and update local converge
+    for (int i = 0; i < g.world_size; i++) {
+        if (i!=g.world_rank) {
+            MPI_Status status;
+            double* recv_buf = new double[1];
+            converge_recv_bufs.push_back(recv_buf);
+
+            MPI_Recv(recv_buf, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, &status); //MPI_SOURCE?
+            diff += recv_buf[0];
+        }
+    }
+
+    //clear converge buf
+    delete(converge_send_buf);
+
+    for (size_t i = 0; i < converge_recv_bufs.size(); i++) {
+        delete(converge_recv_bufs[i]);
+    }
+
+    delete(converge_send_reqs);
+    return diff;
+
+    /*
     // Calculate local convergence
     double diff = 0.;
     #pragma omp parallel for reduction(+:diff)
@@ -85,6 +183,7 @@ double compute_global_diff(DistGraph &g, double *solution, double *old) {
     }
 
     return global_diff;
+    */
 }
 
 
